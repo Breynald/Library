f8e9a4bacf4a99834dff4a2f6a57d8aae1b78dc3
Breynald <1471079796@qq.com>
2021-01-04 21:53:41 +0800

    1

--- C/same.c
+++ C/same.c
@@ -4,5 +4,14 @@
 
 int main()
 {
-    
+    char a[100], b[100];
+    gets(a);
+    gets(b);
+    int n = strlen(a) < strlen(b) ? strlen(a) : strlen(b);
+    for(int i=0; i<n; i++)
+    {
+        if(a[i] == b[i])
+            printf("%c", a[i]);
+    }
+    return 0;
 }
