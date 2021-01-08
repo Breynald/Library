922997f5921e6c66fb2fe5e6c5b5387cc7a66819
Breynald <1471079796@qq.com>
2021-01-08 11:33:23 +0800

    1

--- C/tt.c
+++ C/tt.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-struct bit
+float function(float x)
 {
-	int x;
-	int y;
-	char name[20];
-};
+	return x*x*x;
+}
+
+float Integral(float (*f)(float), float a, float b)
+{
+    float s, h;
+    int n = 100, i;
+    s = ((*f)(a) + (*f)(b)) / 2;
+    h = (b - a) / n;
+     
+    for (i=1; i<n; i++)
+    {
+       s += (*f)(a + i * h);
+    }
+    return s * h ;
+}
+
 
 int main()
 {
-	struct bit a;
-	a.x = 1;
-	a.y = 2;
-	strcpy(a.name, "hello");
-	FILE *p = fopen("aaa.txt", "w");
-	fwrite(&a, sizeof(a), 1, p);
-	fclose(p);
+	
+	printf("%f", Integral(function, 1, 2));
 	return 0;
 }
