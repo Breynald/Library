9eaffe8a1275869e829a7d8c34a1059eed233d89
Breynald <1471079796@qq.com>
2021-01-29 22:32:08 +0800

    2

---
+++ Codeforce/head.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vd vector<double>
+#define vvd vector<vector<double>>
+#define pb push_back
+#define f(n) for(int i=0; i<n; i++)
+#define FIO ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define FRE freopen("in.in", "r+", stdin);freopen("out.out", "w+", stdout)
