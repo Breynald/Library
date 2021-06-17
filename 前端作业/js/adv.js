44139af112fa1ac569e76c75e41110d89244236e
Breynald <1471079796@qq.com>
2021-06-18 00:25:15 +0800

    1

--- 前端作业/js/adv.js
+++ 前端作业/js/adv.js
@@ -1,20 +1,53 @@
-var panel = document.getElementsByClassName("panel");
-
-var len = document.querySelector(".panel_box").childElementCount;
-
-panel[0].style.zIndex = "1";
-panel[1].style.zIndex = "0";
-panel[2].style.zIndex = "0";
-panel[3].style.zIndex = "0";
-panel[0].style.opacity = "1";
-panel[1].style.opacity = "0";
-panel[2].style.opacity = "0";
-panel[3].style.opacity = "0";
-panel[0].style.display = "block";
-panel[1].style.display = "none";
-panel[2].style.display = "none";
-panel[3].style.display = "none";
-
-
-
-
+var currentIndex = 0;
+var id;
+var len = document.querySelector('.carousel').childElementCount;
+function carousel(index){
+    if (currentIndex === len){
+        currentIndex = index = 0;
+    }
+    if (currentIndex < 0){
+        currentIndex = index = 4;
+    }
+    document.querySelector('.current').className = '';
+    document.querySelector('.circle li:nth-child(' + (index + 1) + ')').className = 'current';
+    document.querySelector('.selection').className = '';
+    document.querySelector('.carousel li:nth-child(' + (index + 1) + ')').className = 'selection';
+}
+function prev(){
+    currentIndex--;
+    carousel(currentIndex)
+}
+function next(){
+    currentIndex++;
+    carousel(currentIndex)
+}
+var lis = document.querySelectorAll('.circle li');
+for (var i = 0; i < lis.length; i++){
+    lis[i].index = i;
+    lis[i].onclick = function(){
+        currentIndex = this.index
+        carousel(currentIndex);
+    }
+    var showClothTimer;
+    lis[i].onmouseover = function(){
+        var temp = this.index
+        showClothTimer = setTimeout(function(){
+            currentIndex = temp
+            carousel(currentIndex);
+	    },500);
+    }
+    lis[i].addEventListener( 'mouseout', function() {
+        clearTimeout(showClothTimer);
+    }, false);
+}
+function auto() {
+    id = setInterval(next, 2000)
+}
+function stop() {
+    clearInterval(id)
+}
+auto();
+document.querySelector('.prev').onclick = prev
+document.querySelector('.next').onclick = next
+document.querySelector('.middle_layout').onmouseover = stop
+document.querySelector('.middle_layout').onmouseout = auto
